917aaefa231f75c19192cfd053e943155e89c3b9
wonjong <won72881@naver.com>
2024-02-13 16:43:28 +0900

    first commit - wonjong

---
+++ src/main/java/joljak/homecoming/HomecomingApplication.java
@@ -0,0 +1,13 @@
+package joljak.homecoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecomingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomecomingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/joljak/homecoming/HomecomingApplicationTests.java
@@ -0,0 +1,13 @@
+package joljak.homecoming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecomingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
