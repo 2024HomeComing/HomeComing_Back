e6a2a00b961e209a14a86440d92b622407a934b8
sung0506 <[sung990506@naver.com]>
2024-05-21 18:03:48 +0900

    게시판 수정

--- src/main/java/com/example/crud/controller/BoardController.java
+++ src/main/java/com/example/crud/controller/BoardController.java
@@ -4,9 +4,7 @@ import com.example.crud.entity.Board;
 import com.example.crud.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
